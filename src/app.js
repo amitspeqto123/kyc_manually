8c1f60ae9786c94119679e95c4e5440c91dc88fd
amitspeqto123 <amit.kumargupta@speqto.com>
2026-02-05 13:08:19 +0530

    Signup and Login done between admin and user

---
+++ src/app.js
@@ -0,0 +1,38 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+
+//import authRoutes from "./routes/authRoute.js";
+import authRoute from "./routes/user.route.js"
+//import profileRoute from "./routes/profile.route.js"
+
+
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({ origin: true, credentials: true }));
+app.use(helmet());
+app.use(morgan("dev"));
+
+// app.use(session({
+//   secret: "mysecretkey",
+//   resave: false,
+//   saveUninitialized: false
+// }));
+
+// import "./config/passport.js";
+// app.use(passport.initialize());
+// app.use(passport.session());
+
+// Routes
+app.use("/v1/auth", authRoute);
+//app.use("/v1/profile", profileRoute);
+// send back a 404 error for any unknown api request
+// app.use((req, res, next) => {
+//   next(new ApiError(404, "Not found"));
+// });
+
+export default app;
---
+++ src/middlewares/jwt.js
@@ -0,0 +1,33 @@
+import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
+
+export const isAuthenticated = async (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res.status(401).json({ message: "Token missing or invalid" });
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await User.findById(decoded.userId).select("-password");
+    if (!user) {
+      return res.status(401).json({ message: "User not found" });
+    }
+
+    req.user = user; // yahi se aage sab milega
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+};
+
+export const isAdmin = (req, res, next) => {
+  if (req.user.role !== "ADMIN") {
+    return res.status(403).json({ message: "Admin access only" });
+  }
+  next();
+};
